b8af79c6ff65f3a5619c9a7725d8235813092bca
unknown <david.kaiser.82@gmail.com>
2018-01-19 22:17:48 +0100

    add column names

--- read_btl_DK.R
+++ read_btl_DK.R
@@ -1,32 +1,16 @@
-library(dplyr)
 filename <- "example_data/EMB.btl"
 
-# get the header
-bottle <- readLines(filename)
-header.end <- grep("Btl_Posn", bottle)+1 # defines the line number where text *END* is found.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # get the data
+bottle <- readLines(filename)
+
 data <- read.table(filename, 
-                   sep="", 
-                   dec=".",  
-                   skip=header.end, 
-                   header = FALSE, stringsAsFactors = FALSE,
-                  # col.names=header.df$colnames, 
-                   fill=TRUE, # one or more files have "samples per bin" values missing, this fills the blanks
-                   na.strings=c("", "NA")# defines values as NA
+                   sep = "", 
+                   dec = ".",  
+                   skip = grep("Btl_Posn", bottle)+1, 
+                   header = FALSE, 
+                   stringsAsFactors = FALSE,
+                   fill = TRUE, # one or more files have "samples per bin" values missing, this fills the blanks
+                   na.strings = c("", "NA") # defines values as NA
                   ) 
 
 data <- as.data.frame( # return as a dataframe
@@ -43,10 +27,16 @@ data <- as.data.frame( # return as a dataframe
 
 data <- data[,(max(rowSums(is.na(data)))+1):ncol(data)] # remove rows in the front that contain meta data or NA
 
+# get the column names for the data table
+column.names <- bottle[grep("Btl_Posn", bottle)] # return the line with the column names but in one string with multiple whitespaces
+column.names <- scan(text = column.names, what = "") # read the single string into a vector of strings by multiple whitespaces ("")
+if(any(grepl("FlECO-AFLTurbWETntu0", column.names))) { # test if x is a part of y; in the .btl ther is nos space between the two parameters
+  column.names <- scan(text = gsub("FlECO-AFLTurbWETntu0", "FlECO-AFL TurbWETntu0", column.names), what = "") # replace "FlECO-AFLTurbWETntu0" with "FlECO-AFL TurbWETntu0" and separate it by ""
+} 
+column.names <- column.names[3:length(column.names)] # remove the first two entries int column names, i.e. Btl_Posn and Date
 
-###### example snippets
-line <- data[4,]
-shortline <- line[1:(length(line)-sum(is.na(line)))] # remove the NAs from the line as all an NAs are at the end
-linena <- as.data.frame(t(as.data.frame(rep(NA, sum(is.na(line)))))) # make a dataframe with columns filled with NA 
-newline <- cbind(linena, shortline) # put the NAs in front of the data values
-newline[2,] <- line[1,]
+names(data) <- column.names
+names(data)[ncol(data)] <- "statistic" # this column is not named
+
+# get metadata
+
